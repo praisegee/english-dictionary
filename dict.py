4e2882b4fec72c1347e6f1a08c22da99fbed96c8
gentle <adetoyeseoyekanmi@gmail.com>
2022-08-29 16:47:17 +0100

    My dictionary

--- dict.py
+++ dict.py
@@ -25,10 +25,11 @@ def search():
     get_meaning = entry.get().lower()
     if get_meaning != "":
         try:
-            # text_area.delete("1.0", "end")
-            # text_area.insert(END, data[get_meaning])
+            text_label.delete("1.0", "end")
+            text_label.insert(END, data[get_meaning])
+            text_label.config(state=DISABLED)
             # entry.delete(0, END)
-            text_label.config(text=data[get_meaning])
+            # text_label.config(text=data[get_meaning])
         except KeyError:
             ask = messagebox.askyesno(title="Search Online", message="Sorry Word not found! Would you like to search online")
             if ask:
@@ -56,7 +57,10 @@ def previous_word():
             result = None
         entry.delete(0, END)
         entry.insert(0, result)
-        text_label.config(text=data[result])
+        text_label.config(state=NORMAL)
+        text_label.delete("1.0", END)
+        text_label.insert(END, data[result])
+        text_label.config(state=DISABLED)
     else:
         messagebox.showerror(title="Error", message="Search bar is still empty")
 
@@ -72,14 +76,24 @@ def next_word():
             result = None
         entry.delete(0, END)
         entry.insert(0, result)
-        text_label.config(text=data[result])
+        text_label.config(state=NORMAL)
+        text_label.delete("1.0", END)
+        text_label.insert(END, data[result])
+        text_label.config(state=DISABLED)
     else:
         messagebox.showerror(title="Error", message="Search bar is still empty")
 
 
 def font_size():
-    slider_label.config(text=int(scale_1.get()))
+    slider_label.delete(0, END)
+    slider_label.insert(0, int(scale_1.get()))
     text_label.config(font=("arial", int(scale_1.get())))
+
+
+def binder(event):
+    getter = slider_label.get()
+    scale_1.config(value=int(getter))
+    text_label.config(font=("arial", getter))
 
 
 # label1 = Label(text="A-Z English Dictionary", bg="#458AFF", font=("arial", 15, "normal"))
@@ -97,14 +111,14 @@ entry.insert(0, "Search Here")
 entry.config(state=DISABLED)
 # Bind to function click
 entry.bind("<Button-1>", click)
-entry.pack()
+entry.place(x=12, y=10)
 
 
 # frame = Frame(window)
 # frame.grid(row=0, column=2)
 search_img = PhotoImage(file="Images/Search _Button.png")
 search_btn = Button(image=search_img, borderwidth=0, bg="#458AFF", command=search)
-search_btn.pack()
+search_btn.place(x=616, y=13)
 # Previous and next button
 previous_img = ImageTk.PhotoImage(Image.open("Images/back.jpeg"))
 previous_btn = Button(entry_frame, image=previous_img, relief=GROOVE, bg="#458AFF", command=previous_word)
@@ -113,18 +127,21 @@ next_btn = Button(entry_frame, image=next_img, relief=GROOVE, bg="#458AFF", comm
 previous_btn.pack()
 next_btn.pack()
 
-status_bar = Label(text="", bd=1, relief=GROOVE, anchor=E)
+# status_bar = Label(text="", bd=1, relief=GROOVE, anchor=E)
 # status_bar.grid(row=3, column=0)
 
 scale_1 = ttk.Scale(window, orient="horizontal", length=200, from_=0, to=100, value=10)
 scale_1.pack()
 
-slider_label = Label(text="10", bg="#458AFF")
+slider_label = Entry(width=4)
+num = "10"
+slider_label.insert(0, "10")
 slider_label.pack()
+slider_label.bind("<Return>", binder)
 scale_1.config(command=lambda e: font_size())
 
 
-text_label = Label(text="", anchor="nw", wraplength=790, padx=15, pady=10)
+text_label = Text(padx=15, pady=10)
 text_label.pack(padx=10, pady=10, fill=BOTH, expand=True)
 
 
